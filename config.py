d86d686e044a66d5282351fe947aafa857dff546
jmed1 <52293924+jmed1@users.noreply.github.com>
2023-03-15 19:16:20 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-#Configuration Variables for JIRA project connection. Modify this accordingly.
-Variables = {
-"API_TOKEN" : "ATATT3xFfGF0h6ofIvGVppyWdKPO_FRKgc4DBPhzgcoTKyHDZUYMc04hukbnXnPmZ1w1U-HEq4Ttd1FsdsbXg5Ap4uvcKD3KaFFCGYMcbnLCCy65IVXFaX7Pvc6xTKPd73uCp8FHOeRtLcbECBmlVzw2sOUTJJtMJIaPOX5g5EBXrxdVeBxayZ8=A1E80B41",
-"BASE_URL" : "https://jira-automation-demo.atlassian.net/",
-"EMAIL" : "pepemedrano1@yahoo.com",
-"PROJECT_KEY":"AD"
-}
