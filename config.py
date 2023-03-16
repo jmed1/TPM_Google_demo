6414cae814a44875592186f0279f33e5aa001a3f
jmed1 <52293924+jmed1@users.noreply.github.com>
2023-03-15 19:16:35 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+#Configuration Variables for JIRA project connection. Modify this accordingly.
+Variables = {
+"API_TOKEN" : "ATATT3xFfGF0eNneo4gx0zKCvnd-r3R-cT239uT7dgxYQaSFufsKRlu890kt6ZADgLnVxNXAkqeaNiHJMCaMyd-y0dH7PQeDy9nmwnEbQa60I4AHL3mmP5Nt0m6BKS9UQCZ8fl5zbNMIsbu2jiAuxbLhZIs5w7fMYhfhcONSH-SyyoJw3eT7XcI=C23C8646",
+"BASE_URL" : "https://jira-automation-demo.atlassian.net/",
+"EMAIL" : "pepemedrano1@yahoo.com",
+"PROJECT_KEY":"AD"
+}
